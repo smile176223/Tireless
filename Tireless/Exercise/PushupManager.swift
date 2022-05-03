7f2acdcbf4e74c837d72fc2aeebe54aad7c9569e
LiamHao <smile176223@gmail.com>
2022-05-04 02:32:42 +0800

    fix pushup manager bug

--- Tireless/Exercise/PushupManager.swift
+++ Tireless/Exercise/PushupManager.swift
@@ -23,13 +23,21 @@ class PushupManager {
     
     private var startPointDown = CGPoint(x: 0, y: 0)
     
+    func resetIfOut() {
+        startPointUp = CGPoint(x: 0, y: 0)
+        startPointDown = CGPoint(x: 0, y: 0)
+        checkPointA = false
+        checkPointB = false
+        checkPointC = true
+    }
+    
     func pushupWork(_ posePoint: [PosePoint]) -> Int {
+        if checkInFrameLikeHood(posePoint) == false {
+            resetIfOut()
+        }
         let hipToShoulder = posePoint[22].position.y - posePoint[32].position.y
         let wristToShoulder = posePoint[5].position.y - posePoint[32].position.y
         startPointUp = getPolygonAreaCenter(posePoint)
-        print("hip", hipToShoulder)
-        print("wrist", wristToShoulder)
-        print("startUp", startPointUp)
         if hipToShoulder > 0.10, hipToShoulder < 0.30, wristToShoulder > 0.10,
            startPointUp.y < 0.5, startPointUp.x > 0.1, startPointUp.x < 0.75 {
             if checkPointA == false, checkPointB == false, checkPointC == true {
@@ -65,20 +73,31 @@ class PushupManager {
         var yPointSum: CGFloat = 0
         var areaSum: CGFloat = 0
         var point1 = posePoint[1].position
-        for index in 0..<posePoint.count {
-            let point2 = posePoint[index].position
-            let area = area(from: posePoint[0].position, to: point1, end: point2)
+        var tempPoint = posePoint
+        tempPoint.remove(at: 32)
+        tempPoint.remove(at: 30)
+        tempPoint.remove(at: 22)
+        tempPoint.remove(at: 3)
+        for index in 0..<tempPoint.count {
+            let point2 = tempPoint[index].position
+            let area = area(from: tempPoint[0].position, to: point1, end: point2)
             areaSum += area
-            xPointSum += (posePoint[0].position.x + point1.x + point2.x) * area
-            yPointSum += (posePoint[0].position.y + point1.y + point1.y) * area
+            xPointSum += (tempPoint[0].position.x + point1.x + point2.x) * area
+            yPointSum += (tempPoint[0].position.y + point1.y + point1.y) * area
             point1 = point2
         }
         return CGPoint(x: (xPointSum / areaSum / 3), y: (yPointSum / areaSum / 3))
     }
     
     private func area(from pointA: CGPoint, to pointB: CGPoint, end pointC: CGPoint) -> CGFloat {
-        let area = (pointA.x * pointB.y) + (pointA.x * pointC.y) + (pointC.x * pointA.y) -
+        let area = (pointA.x * pointB.y) + (pointB.x * pointC.y) + (pointC.x * pointA.y) -
         (pointB.x * pointA.y) - (pointC.x * pointB.y) - (pointA.x * pointC.y)
         return abs(area) * 0.5
+    }
+    
+    private func checkInFrameLikeHood(_ posePoint: [PosePoint]) -> Bool {
+        return posePoint.allSatisfy { posePoint in
+            posePoint.inFrameLikelihood > 0.8
+        }
     }
 }
