21001629943a37c2de88e18b6d8b4eec57f2d36a
LiamHao <smile176223@gmail.com>
2022-05-06 18:54:54 +0800

    fix group plan join users cell layout

--- Tireless/Controller/GroupPlan/GroupPlanViewController.swift
+++ Tireless/Controller/GroupPlan/GroupPlanViewController.swift
@@ -154,8 +154,8 @@ class GroupPlanViewController: UIViewController {
         
         let innergroup =
         NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 1)
-        let nestedGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.22),
-                                                     heightDimension: .fractionalHeight(0.09))
+        let nestedGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                     heightDimension: .absolute(50))
         let nestedGroup = NSCollectionLayoutGroup.horizontal(layoutSize: nestedGroupSize, subitems: [innergroup])
         let section = NSCollectionLayoutSection(group: nestedGroup)
         
