1e6a89ecb1361cc49b54d155eea51fd5735630f0
LiamHsu <s459958@gmail.com>
2023-11-16 15:40:38 +0800

    Create empty SignupView

---
+++ Tireless/Auth/View/SignupView.swift
@@ -0,0 +1,20 @@
+//
+//  SignupView.swift
+//  Tireless
+//
+//  Created by Liam on 2023/11/16.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignupView()
+    }
+}
