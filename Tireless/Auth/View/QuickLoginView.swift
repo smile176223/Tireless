53eb64ef9f3d14fa39d5aaf04e783400e816aa6c
LiamHsu <s459958@gmail.com>
2023-11-17 17:09:40 +0800

    Add VStack to render it in one preview

--- Tireless/Auth/View/QuickLoginView.swift
+++ Tireless/Auth/View/QuickLoginView.swift
@@ -12,26 +12,27 @@ struct QuickLoginView: View {
     let width: CGFloat
     
     var body: some View {
-        Text("- Or sign up with -")
-            .font(.system(.body))
-            .foregroundColor(.gray)
-            .padding(.bottom, 20)
-        
-        HStack(spacing: 15) {
-            let buttonWidth = (width - 40) / 3
-            let buttonHeight = buttonWidth * 0.6
-            IconButton(iconImage: "star.fill", size: CGSize(width: buttonWidth, height: buttonHeight)) {
-                print("tap button 1")
+        VStack {
+            Text("- Or sign up with -")
+                .font(.system(.body))
+                .foregroundColor(.gray)
+                .padding(.bottom, 20)
+            
+            HStack(spacing: 15) {
+                let buttonWidth = (width - 40) / 3
+                let buttonHeight = buttonWidth * 0.6
+                IconButton(iconImage: "star.fill", size: CGSize(width: buttonWidth, height: buttonHeight)) {
+                    print("tap button 1")
+                }
+                IconButton(iconImage: "apple.logo", size: CGSize(width: buttonWidth, height: buttonHeight)) {
+                    print("tap button 2")
+                }
+                IconButton(iconImage: "star", size: CGSize(width: buttonWidth, height: buttonHeight)) {
+                    print("tap button 3")
+                }
             }
-            IconButton(iconImage: "apple.logo", size: CGSize(width: buttonWidth, height: buttonHeight)) {
-                print("tap button 2")
-            }
-            IconButton(iconImage: "star", size: CGSize(width: buttonWidth, height: buttonHeight)) {
-                print("tap button 3")
-            }
+            .padding(.bottom, 30)
         }
-        .padding(.bottom, 30)
-        
     }
 }
 
