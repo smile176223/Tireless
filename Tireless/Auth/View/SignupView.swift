7773d4057260abe0c2e2eaac1a35c35646a14522
LiamHsu <s459958@gmail.com>
2023-11-17 17:02:30 +0800

    Create QuickLoginView to reduce duplicate

--- Tireless/Auth/View/SignupView.swift
+++ Tireless/Auth/View/SignupView.swift
@@ -39,66 +39,72 @@ struct SignupView: View {
                     .frame(width: 150, height: 150, alignment: .center)
                     .scaledToFill()
                     .colorMultiply(.brown)
+            
+                Text("Create your Account")
+                    .font(.system(.title3))
+                    .foregroundColor(.gray)
+                    .bold()
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    .lineLimit(1)
+                    .truncationMode(.tail)
+                    .padding(.leading, 5)
+                    .padding(.bottom, 10)
                 
-                Group {
-                    Text("Create your Account")
-                        .font(.system(.title3))
-                        .foregroundColor(.gray)
-                        .bold()
-                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                        .lineLimit(1)
-                        .truncationMode(.tail)
-                        .padding(.leading, 5)
-                        .padding(.bottom, 10)
-                    
-                    ThemeTextField($email, width: width, placeholder: "Email")
-                    ThemeTextField($password, width: width, placeholder: "Password", isSecure: true)
-                    ThemeTextField($confirmPassword, width: width, placeholder: "Confirm Password", isSecure: true)
-                    
-                    Button {
-                        print("Tap Sign up")
-                    } label: {
-                        Text("Sign up")
-                            .padding(.top, 20)
-                            .padding(.bottom, 20)
-                            .frame(minWidth: width, maxWidth: .infinity,  minHeight: 44)
-                            .font(Font.title3.bold())
-                            .background(RoundedRectangle(cornerRadius: 12).fill(.brown))
-                            .foregroundColor(Color.white)
-                            .cornerRadius(8)
-                    }
-                    .buttonStyle(GrowingButton())
-                    .lineLimit(2)
-                    .multilineTextAlignment(.center)
-                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
-                    .padding(.bottom, 40)
-                }
+                ThemeTextField($email, width: width, placeholder: "Email")
+                ThemeTextField($password, width: width, placeholder: "Password", isSecure: true)
+                ThemeTextField($confirmPassword, width: width, placeholder: "Confirm Password", isSecure: true)
                 
-                Group {
-                    Text("- Or sign up with -")
-                        .font(.system(.body))
-                        .foregroundColor(.gray)
+                Button {
+                    print("Tap Sign up")
+                } label: {
+                    Text("Sign up")
+                        .padding(.top, 20)
                         .padding(.bottom, 20)
-                    
-                    HStack(spacing: 15) {
-                        let buttonWidth = (width - 40) / 3
-                        let buttonHeight = buttonWidth * 0.6
-                        IconButton(iconImage: "star.fill", size: CGSize(width: buttonWidth, height: buttonHeight)) {
-                            print("tap button 1")
-                        }
-                        IconButton(iconImage: "apple.logo", size: CGSize(width: buttonWidth, height: buttonHeight)) {
-                            print("tap button 2")
-                        }
-                        IconButton(iconImage: "star", size: CGSize(width: buttonWidth, height: buttonHeight)) {
-                            print("tap button 3")
-                        }
-                    }
-                    .padding(.bottom, 30)
+                        .frame(minWidth: width, maxWidth: .infinity,  minHeight: 44)
+                        .font(Font.title3.bold())
+                        .background(RoundedRectangle(cornerRadius: 12).fill(.brown))
+                        .foregroundColor(Color.white)
+                        .cornerRadius(8)
                 }
+                .buttonStyle(GrowingButton())
+                .lineLimit(2)
+                .multilineTextAlignment(.center)
+                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
+                .padding(.bottom, 40)
+                
+                QuickLoginView(width: width)
             }
             .padding(.bottom, 20)
         }
         .frame(width: width)
+    }
+}
+
+struct QuickLoginView: View {
+    
+    let width: CGFloat
+    
+    var body: some View {
+        Text("- Or sign up with -")
+            .font(.system(.body))
+            .foregroundColor(.gray)
+            .padding(.bottom, 20)
+        
+        HStack(spacing: 15) {
+            let buttonWidth = (width - 40) / 3
+            let buttonHeight = buttonWidth * 0.6
+            IconButton(iconImage: "star.fill", size: CGSize(width: buttonWidth, height: buttonHeight)) {
+                print("tap button 1")
+            }
+            IconButton(iconImage: "apple.logo", size: CGSize(width: buttonWidth, height: buttonHeight)) {
+                print("tap button 2")
+            }
+            IconButton(iconImage: "star", size: CGSize(width: buttonWidth, height: buttonHeight)) {
+                print("tap button 3")
+            }
+        }
+        .padding(.bottom, 30)
+        
     }
 }
 
