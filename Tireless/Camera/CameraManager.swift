e5dc60a9079b1de866edc95a84b5c5f1fdbe0ab7
LiamHsu <s459958@gmail.com>
2024-01-16 08:59:03 +0800

    Create CameraManager to implement camera video perview

---
+++ Tireless/Camera/CameraManager.swift
@@ -0,0 +1,121 @@
+//
+//  CameraManager.swift
+//  Tireless
+//
+//  Created by Liam on 2024/1/16.
+//
+
+import UIKit
+import AVFoundation
+
+public final class CameraManager: ObservableObject {
+    
+    enum Status {
+        case configured
+        case unConfigured
+        case unauthorized
+        case failed
+    }
+    
+    @Published var error: CameraError?
+    private var status = Status.unConfigured
+    let session = AVCaptureSession()
+    private let videoOutput = AVCaptureVideoDataOutput()
+    private let sessionQueue = DispatchQueue(label: "com.LiamHsu.tireless.sessionQueue")
+    
+    public init() {
+        configure()
+    }
+    
+    private func set(error: CameraError?) {
+        DispatchQueue.main.async {
+            self.error = error
+        }
+    }
+    
+    private func checkPermissions() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            sessionQueue.suspend()
+            AVCaptureDevice.requestAccess(for: .video) { authorized in
+                if !authorized {
+                    self.status = .unauthorized
+                    self.set(error: .deniedAuthorization)
+                }
+                self.sessionQueue.resume()
+            }
+        case .restricted:
+            status = .unauthorized
+            set(error: .restrictedAuthorization)
+        case .denied:
+            status = .unauthorized
+            set(error: .deniedAuthorization)
+        case .authorized:
+            break
+        @unknown default:
+            status = .unauthorized
+            set(error: .unknownAuthorization)
+        }
+    }
+    
+    private func configure() {
+        checkPermissions()
+        
+        sessionQueue.async {
+            self.configureCaptureSession()
+            self.session.startRunning()
+        }
+    }
+    
+    private func configureCaptureSession() {
+        guard status == .unConfigured else { return }
+        
+        session.beginConfiguration()
+        
+        defer {
+            session.commitConfiguration()
+        }
+        
+        let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
+        guard let device = device else {
+            set(error: .cameraUnavailable)
+            status = .failed
+            return
+        }
+        
+        do {
+            let videoInput = try AVCaptureDeviceInput(device: device)
+            if session.canAddInput(videoInput) {
+                session.addInput(videoInput)
+            } else {
+                set(error: .cannotAddInput)
+                status = .failed
+                return
+            }
+        } catch {
+            set(error: .createCaptureInput(error))
+            status = .failed
+            return
+        }
+        
+        if session.canAddOutput(videoOutput) {
+            session.addOutput(videoOutput)
+            
+            videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+            let videoConnection = videoOutput.connection(with: .video)
+            videoConnection?.videoOrientation = .portrait
+        } else {
+            set(error: .cannotAddOutput)
+            status = .failed
+            return
+        }
+        
+        status = .configured
+    }
+    
+    func set(_ delegate: AVCaptureVideoDataOutputSampleBufferDelegate, queue: DispatchQueue) {
+        sessionQueue.async {
+            self.videoOutput.setSampleBufferDelegate(delegate, queue: queue)
+        }
+    }
+}
