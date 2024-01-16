8ac7a44a2665cda0c115c23a291b04d116f2f477
LiamHsu <s459958@gmail.com>
2024-01-16 09:00:38 +0800

    Create CameraView to render camera video preview

--- Tireless/Camera/CameraView.swift
+++ Tireless/Camera/CameraView.swift
@@ -5,66 +5,35 @@
 //  Created by Liam on 2024/1/15.
 //
 
+import SwiftUI
 
-import UIKit
-import AVFoundation
-
-public final class CameraManager: ObservableObject {
+struct FrameView: View {
+    var image: CGImage?
     
-    enum Status {
-        case configured
-        case unConfigured
-        case unauthorized
-        case failed
-    }
+    private let label = Text("Video feed")
     
-    @Published var status = Status.unConfigured
-    
-    let session = AVCaptureSession()
-    var videoDeviceInput: AVCaptureDeviceInput?
-    private let sessionQueue = DispatchQueue(label: "com.LiamHsu.Tireless.captureSessionQueue")
-    
-    public func configureCaptureSession() {
-        sessionQueue.async { [weak self] in
-            guard let self = self, self.status == .unConfigured else { return }
-            
-            self.session.beginConfiguration()
-            self.session.sessionPreset = .hd1280x720
-            
-            self.setupVideoInput()
-            
+    var body: some View {
+        if let image = image {
+            GeometryReader { geometry in
+                Image(image, scale: 1.0, orientation: .upMirrored, label: label)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
+                    .clipped()
+            }
+        } else {
+            EmptyView()
         }
     }
+}
+
+public struct CameraView: View {
+    @StateObject private var model = CameraViewModel()
     
-    private func setupVideoInput() {
-        do {
-            let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
-            
-            guard let device = device else {
-                status = .unConfigured
-                session.commitConfiguration()
-                return
-            }
-            
-            let videoInput = try AVCaptureDeviceInput(device: device)
-            if session.canAddInput(videoInput) {
-                session.addInput(videoInput)
-                videoDeviceInput = videoInput
-                status = .configured
-            } else {
-                status = .unConfigured
-                session.commitConfiguration()
-                return
-            }
-        } catch {
-            status = .failed
-            session.commitConfiguration()
-            return
+    public var body: some View {
+        ZStack {
+            FrameView(image: model.frame)
+                .edgesIgnoringSafeArea(.all)
         }
-    }
-    
-    private func setVideoOutput() {
-        let videoOutput = AVCaptureVideoDataOutput()
-        videoOutput.setSampleBufferDelegate(self, queue: <#T##dispatch_queue_t?#>)
     }
 }
