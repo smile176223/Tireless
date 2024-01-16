07df656f21aa233b2d73e7894dd798ddc919834f
LiamHsu <s459958@gmail.com>
2024-01-16 08:57:52 +0800

    Create CameraViewModel

---
+++ Tireless/Camera/CameraViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  CameraViewModel.swift
+//  Tireless
+//
+//  Created by Liam on 2024/1/16.
+//
+
+import CoreImage
+import Combine
+
+public class CameraViewModel: ObservableObject {
+    @Published var error: Error?
+    @Published var frame: CGImage?
+    
+    private let context = CIContext()
+    private let frameManager = FrameManager()
+    private var cancellables = Set<AnyCancellable>()
+    
+    public init() {
+        setupSubscriptions()
+    }
+    
+    public func setupSubscriptions() {
+        frameManager.cameraManager.$error
+            .receive(on: RunLoop.main)
+            .map { $0 }
+            .assign(to: &$error)
+        
+        frameManager.$current
+            .receive(on: RunLoop.main)
+            .compactMap { buffer in
+                guard let image = CGImage.create(from: buffer) else { return nil }
+                
+                var ciImage = CIImage(cgImage: image)
+                
+                return self.context.createCGImage(ciImage, from: ciImage.extent)
+            }
+            .assign(to: &$frame)
+    }
+}
